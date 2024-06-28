88f4923f3e166e375222ff46c1d1220332d0ce15
Angela Torres <aura@MacBook-Air-de-Aura.local>
2024-06-27 21:16:21 -0500

    se agrego resect.css

--- ejercicios_f/stack.ts
+++ ejercicios_f/stack.ts
@@ -24,31 +24,27 @@ class Persona {
            return this.profesion;
        }
    }
-   //implemetacion stack
+   
+   // Implementación de la clase Stack
    class Stack<T> {
        private items: T[] = [];
    
-      
        push(element: T): void {
            this.items.push(element);
        }
    
-     
        pop(): T | undefined {
            return this.items.pop();
        }
    
-      
        peek(): T | undefined {
            return this.items[this.items.length - 1];
        }
    
-      
        isEmpty(): boolean {
            return this.items.length === 0;
        }
    
-      
        size(): number {
            return this.items.length;
        }
@@ -58,32 +54,33 @@ class Persona {
        }
    }
    
-   
-   
+   // Creación de una instancia de Stack para personas
    const personaStack = new Stack<Persona>();
    
-   personaStack.push(new Persona(30, 'jhon', 'Pérez', 'Ingeniero'));
-   personaStack.push(new Persona(25, 'estefa', 'Gómez', 'Doctora'));
-   personaStack.push(new Persona(28, 'amelia', 'torres', 'Desarrolladora de software'));
+   personaStack.push(new Persona(30, 'Jhon', 'Pérez', 'Ingeniero'));
+   personaStack.push(new Persona(25, 'Estefa', 'Gómez', 'Doctora'));
+   personaStack.push(new Persona(28, 'Amelia', 'Torres', 'Desarrolladora de software'));
    personaStack.push(new Persona(30, 'Juan', 'Pérez', 'Ingeniero'));
    personaStack.push(new Persona(25, 'María', 'Gómez', 'Doctora'));
-   personaStack.push(new Persona(28, 'Angela', 'torres', 'Desarrolladora de software'));
+   personaStack.push(new Persona(28, 'Angela', 'Torres', 'Desarrolladora de software'));
    
-   
-   let personaa = personaStack.peek();
-   
-   if (personaa) {
-       console.log(${personaa.getNombre()} ${personaa.getApellido()}, ${personaa.getProfesion()}); 
+   // Mostrar la última persona en la pila (peek)
+   let persona = personaStack.peek();
+   if (persona) {
+       console.log(`${persona.getNombre()} ${persona.getApellido()}, ${persona.getProfesion()}`);
    }
    
-   personaa = personaStack.pop();
-   if (personaa) {
-       console.log(${personaa.getNombre()} ${personaa.getApellido()}, ${personaa.getProfesion()});
+   // Sacar y mostrar la persona eliminada de la pila (pop)
+   persona = personaStack.pop();
+   if (persona) {
+       console.log(`${persona.getNombre()} ${persona.getApellido()}, ${persona.getProfesion()}`);
+   }
    
-   console.log(personaStack.size()); 
-   console.log(personaStack.isEmpty()); 
+   // Mostrar el tamaño actual de la pila y si está vacía
+   console.log(personaStack.size());
+   console.log(personaStack.isEmpty());
    
+   // Limpiar la pila y verificar si está vacía después de limpiar
    personaStack.clear();
-   console.log(personaStack.isEmpty()); 
+   console.log(personaStack.isEmpty());
    
-   }
